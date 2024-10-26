69885d9dbcd2677da7067276211cbff9eb714a42
soonyubing <soonyubi1@naver.com>
2024-10-26 15:46:06 +0900

    feat: add prisma provider

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { PrismaService } from './common/prisma.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [PrismaService],
+  exports: [PrismaService],
 })
 export class AppModule {}
